74092912cb45ff397fbde9c63c5224a5c5e13fb5
JEROME <jerome.bonacchi@etu.upmc.fr>
2019-04-17 11:52:59 +0200

    window2

--- window.py
+++ window.py
@@ -1,19 +1,10 @@
 from easygui import *
 
+#http://easygui.sourceforge.net/tutorial.html#enterbox
 #http://hebergement.u-psud.fr/iut-orsay/Pedagogie/MPHY/Python/easygui.pdf
 
-# msgbox("Ce moi de novembre est particulièrement pluvieux pour le sud de la France",\
-#  title="Chatbot", ok_button= "Tout à fait",\
-#  image="/nfs/home/sasl/eleves/main/3670819/Informatique/Python/TP1/giphy.gif")
-#
-# buttonbox(msg="Python est intéressant ?", title="Chatbot",\
-# 	choices=("Tout à fait d'accord", "D'accord","Pas d'accord","Pas du tout d'accord"),\
-# 	image = None)
-# valeurs = multenterbox(msg="Veuillez saisir l'adresse", title="Chatbot",\
-# 	fields=("Query"),\
-# 	values=())# requete
-# print(valeurs)
-
-enterbox(msg = "Enter your query", title = "Chatbot", default = "")
+title = "Chatbot"
+query = enterbox(msg = "Enter your query", title = title, default = "")
+message = "Your query was \""+ query + "\".\nThe result of your query is :"
 text = "Hello"
-textbox(msg = "Result of your query", title = "Chatbot", text = text)
+textbox(msg = message, title = title, text = text)
