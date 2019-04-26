ab7609b0ac4b2d502b6f5e5439433387ddcf44d0
JEROME <jerome.bonacchi@etu.upmc.fr>
2019-04-26 12:06:08 +0200

    testOnto v2

--- testOnto.py
+++ testOnto.py
@@ -4,4 +4,15 @@ path = "/home/jerome/Informatique/chatbot/Ontology/"
 file = "testOntologyGoT.owl"
 onto = get_ontology("file://" + path + file).load()
 
-print(House.__class__)
+print("\nListe des classes :\n",list(onto.classes()))
+print("\nListe des instances :\n",list(onto.individuals()))
+print("\nListe des propriétés :\n",list(onto.object_properties()))
+
+print("\nListe des sous-classes de Place :\n",\
+onto.search(subclass_of = onto.Place))
+print("\nListe des entites avec le mot desire :\n",\
+onto.search(iri = "*City"))
+print("\nListe des instances de Place :\n",\
+onto.search(type = onto.Place))
+print("\nListe des entites avec la relation desiree :\n",\
+onto.search(isFromHouse = "*"))
