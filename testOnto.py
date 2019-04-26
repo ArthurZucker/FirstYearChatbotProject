20dee4e933c28bf37fea70ba1ed4fd6eca79a1f2
JEROME <jerome.bonacchi@etu.upmc.fr>
2019-04-26 11:31:13 +0200

    index

--- testOnto.py
+++ testOnto.py
@@ -1,6 +1,6 @@
 from owlready2 import *
 #/path/to/your/local/ontology/repository
-onto_path.append("~/Informatique/chatbot")
+onto_path.append("~/Informatique/chatbot/Ontology")
 onto = get_ontology("http://www.semanticweb.org/jerome/ontologies/2019/3/untitled-ontology-6")
 onto.load()
 
