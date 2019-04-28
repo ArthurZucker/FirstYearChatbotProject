846affdc32fdb4b683be2a2178e009515296085a
JEROME <jerome.bonacchi@etu.upmc.fr>
2019-04-28 14:34:29 +0200

    rename2

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,10 @@
-import src/testOnto
+import src/onto
 import src/window
+
+# still = 1
+# while still == 1:
+#   mode = selectModeBox()
+#   cancel, query, message = queryBox(mode)
+#   answer = function(query)
+#   answerBox(cancel, query, message, answer)
+#   still = endBox()
