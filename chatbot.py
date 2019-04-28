c6c4cd5674130d7c1b10af9f7f9977ee98cbec1e
JEROME <jerome.bonacchi@etu.upmc.fr>
2019-04-28 14:40:08 +0200

    exe

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,15 @@
-import src/onto
-import src/window
+import src/onto     # pour charger l ontologie
+import src/window   # pour charger l interface graphique
+# import src/       # fichier de Younes
+# import src/       # fichier de Romain
 
 # still = 1
 # while still == 1:
 #   mode = selectModeBox()
 #   cancel, query, message = queryBox(mode)
-#   answer = function(query)
+#
+#   answer = function(query)  # function est tout ce qui traite la requete et
+#                               renvoie la reponse
+#
 #   answerBox(cancel, query, message, answer)
 #   still = endBox()
