6418747e4db1849de02e38525727d9494d76dc92
JEROME <jerome.bonacchi@etu.upmc.fr>
2019-05-03 16:08:31 +0200

    deplacement

--- source/reply.py
+++ source/reply.py
@@ -40,3 +40,16 @@ def reply(mode, query, ontology):
   # ...
 	answer = ""
 	return answer
+
+def elem_question(question, GoT):
+	if (question.find(",") == 2 and question.find("?") == 1):
+		question.replace("?","")
+		x = question.split(",")
+		if (x[0] in list(GoT.individuals()) and x[1] in list(GoT.properties()) and x[2] in list(GoT.individuals())):
+			return x
+		else:
+			errorbox()
+			return None
+	else:
+		errorbox()
+		return None
